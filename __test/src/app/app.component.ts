9c75f2583acfec2a9e3db1d81f8ff949bd9d82c8
Chesney Nyffels <chesney@nyffels.be>
2024-11-22 16:52:20 +0100

    Re-added __test

---
+++ __test/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { translate } from '@jsverse/transloco';
+import { defaultMarker } from '@nyffels/transloco-extractor-defaults/marker';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'trans-test';
+
+  constructor(){
+    translate("test.0")
+    translate(defaultMarker("test.1"));
+    translate(defaultMarker('test.2', 'test'));
+  }
+}
